8b32912ba17ed1698ded00ca7f2a9201b731f46f
maibui7879 <maibui7879@gmail.com>
2025-10-22 20:11:31 +0700

    gay

--- app/src/main/java/com/example/flashcard/ui/MainActivity.java
+++ app/src/main/java/com/example/flashcard/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.flashcard;
+package com.example.flashcard.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.flashcard.R;
 
 public class MainActivity extends AppCompatActivity {
 
